8faee97fb3b738b5d84aced6c1d18cf3f13265cc
Rkay Ji <mrrkayj86@gmail.com>
2025-10-04 11:10:31 +0530

    updaing s speed monitor

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
