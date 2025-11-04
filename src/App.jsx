dc819dbd551998cfff14a1b656244e63cab2e98d
Rohit <59389316+rkayji86@users.noreply.github.com>
2025-11-04 20:06:00 +0530

    Update main app background to use black in dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer'
 function App() {
 
   return (
-    <div className='min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300'>
+    <div className='min-h-screen bg-white dark:bg-black transition-colors duration-300'>
       <Navigation />
       <HeroSection />
       <AboutSection />
